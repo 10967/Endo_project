ebf0a0a947e09f52300d7c1de0ede10d73a38739
Malak <arhnouchm@gmail.com>
2026-04-01 17:04:11 +0100

    fix: restore test BF-4 commit1

--- app/src/test/java/com/spmenais/paincare/Models/UsersTest.java
+++ app/src/test/java/com/spmenais/paincare/Models/UsersTest.java
@@ -30,7 +30,7 @@ public class UsersTest {
     @Test
     public void constructeurQuatreParams_assigneTousLesChamps() {
         Users user = new Users("u1", "Malak", "malak@test.com", "http://img.jpg");
-        assertEquals("ERREUR", user.getUserId());;
+        assertEquals("u1", user.getUserId());
         assertEquals("Malak", user.getName());
         assertEquals("malak@test.com", user.getEmail());
         assertEquals("http://img.jpg", user.getImageUrl());
